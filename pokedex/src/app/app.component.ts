a6abc222fe6d77473048d957870a7c3625d35cda
jocelynboitelle <jocelynboitelle@gmail.com>
2019-08-15 17:38:40 +0200

    delete color by type

--- pokedex/src/app/app.component.ts
+++ pokedex/src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pokemon } from './pokemon.model';
 import { data } from './pokemon.data';
 import { Criteria } from './filter/criteria.model';
-import { BgType } from './filter/type.model';
-import { bgTypes } from './filter/type.data';
 
 @Component({
   selector: 'app-root',
@@ -13,33 +11,17 @@ import { bgTypes } from './filter/type.data';
 export class AppComponent implements OnInit {
 
   listPokemons: Pokemon[];
-  types: BgType[];
-  bgt: String;
 
   ngOnInit(): void {
     this.listPokemons = data;
-    this.types = bgTypes;
-  }
-
-  backgroundType(criteria: Criteria): string {
-    this.types = bgTypes.filter((bgType: BgType) => this.isType(criteria, bgType));
-    
-    return this.types[0].img;
-  }
-
-  isType(criteria: Criteria, bgType: BgType): boolean {
-    const thisType = !criteria || criteria.type === bgType.type;
-    return thisType;
   }
 
   filterList(criteria: Criteria) {
     this.listPokemons = data.filter((pokemon: Pokemon) => this.futurData(criteria, pokemon));
-    this.bgt = this.backgroundType(criteria);
-    console.log(this.bgt);
   }
 
   futurData(criteria: Criteria, pokemon: Pokemon): boolean {
-    
+
     const pokemonId = parseInt(pokemon.id, 10);
     const criteriaId = parseInt(criteria.id, 10);
 
